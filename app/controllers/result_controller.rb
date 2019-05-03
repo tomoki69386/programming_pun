a5d750c8000d5423fa64867885f34f25675f8815
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-05-03 19:04:36 +0900

    Update result

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,5 +1,5 @@
 class ResultController < ActionController::Base
   def show
-    @result = ["dockerーのファイルがどっかーいったーーーーーーー"].sample
+    @result = ["dockerーのファイルがどっかーいったーーーーーーー", "整数型でいint違う？", "Javaは邪魔"].sample
   end
 end
